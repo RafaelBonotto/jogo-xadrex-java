61223ef482179cc2d180caaa2f91db682c205fb7
RafaelBonotto <rafareisaba@gmail.com>
2020-08-04 22:05:39 -0300

    Classe XadrezExcepition e XadrezPosicao

---
+++ src/xadrez/XadrezExcepition.java
@@ -0,0 +1,9 @@
+package xadrez;
+
+public class XadrezExcepition extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public XadrezExcepition(String msg){
+        super(msg);
+    }
+}
